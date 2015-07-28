f3281fa43457f285e594ec1507fbfb6c90fae9b3
justin@polyformlabs.com <todayTomorrowBeaverTail>
2015-07-28 09:48:07 -0400

    updated drawer to close on click angular maps for points of interest owl gallery node virtual hosts for multiple websites

--- public/assets/js/clientApp.js
+++ public/assets/js/clientApp.js
@@ -1,10 +1,10 @@
 
-var mainMod= angular.module('SandJ', ['ui.router']);
+var mainMod= angular.module('SandJ', ['ui.router','uiGmapgoogle-maps']);
 
 console.log("in clientapp main");
 
 
-mainMod.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+mainMod.config(['$stateProvider','$urlRouterProvider','uiGmapGoogleMapApiProvider',function($stateProvider,$urlRouterProvider,uiGmapGoogleMapApiProvider) {
 
     console.log("in app config");
     $stateProvider.state('home', {
@@ -25,19 +25,135 @@ mainMod.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$u
         controller:'ceremonyCtrl'
     });
 
+    $stateProvider.state('contact', {
+        url: "/contact",
+        templateUrl: "assets/js/partials/contact.html",
+        controller:'contactCtrl'
+    });
+
+    $stateProvider.state('news', {
+        url: "/news",
+        templateUrl: "assets/js/partials/news.html",
+        controller:'newsCtrl'
+    });
+
+    $stateProvider.state('gallery', {
+        url: "/gallery",
+        templateUrl: "assets/js/partials/gallery.html",
+        controller:'galleryCtrl'
+    });
+    $stateProvider.state('points-of-interest', {
+        url: "/points-of-interest",
+        templateUrl: "assets/js/partials/points-of-interest.html",
+        controller:'pointsOfInterestCtrl'
+    });
     $urlRouterProvider.otherwise('/');
 
+
+    uiGmapGoogleMapApiProvider.configure({
+
+    });
+
+}]);
+
+
+mainMod.controller('baseCtrl',['$scope','$state',function($scope,$state){
+    console.log("BASE CTRL");
+    $scope.closeDrawer= function(){
+      console.log("close drawer...");
+        var sjDrawer = angular.element(document.getElementById("sj-drawer-container"));
+        sjDrawer.removeClass("is-visible");
+
+    };
 }]);
 
 
 mainMod.controller('homeCtrl',['$scope','$state',function($scope,$state){
     console.log("HOME CTRL");
-
-
 }]);
-
 
 mainMod.controller('ceremonyCtrl',['$scope','$state',function($scope,$state){
     console.log("CEREMONY CTRL");
+
+}]);
+
+mainMod.controller('contactCtrl',['$scope','$state',function($scope,$state){
+    console.log("CONTACT CTRL");
+}]);
+
+mainMod.controller('newsCtrl',['$scope','$state',function($scope,$state){
+    console.log("NEWS CTRL");
+}]);
+
+mainMod.controller('galleryCtrl',['$scope','$state',function($scope,$state){
+    console.log("GALLERY CTRL");
+    angular.element(document).ready(function(){
+        $("#sj-gallery-container").owlCarousel({
+            lazyLoad : true,
+            autoPlay : 6000,
+            stopOnHover : true,
+            navigation:false,
+            paginationSpeed : 1000,
+            paginationNumbers:true,
+            goToFirstSpeed : 2000,
+            singleItem : true,
+            autoHeight : true,
+            transitionStyle:"fade"
+        });
+    });
+}]);
+
+
+mainMod.controller('pointsOfInterestCtrl',['$scope','$state','uiGmapGoogleMapApi',function($scope,$state,uiGmapGoogleMapApi){
+    console.log("POINTS OF INTEREST CTRL");
+    $scope.map = { center: { latitude: -32.999432, longitude: 27.9354535 }, zoom: 10 };
+
+
+    $scope.gmpMarkers = [{
+        id:"1",
+        latitude:-32.998616,
+        longitude:  27.927984,
+        title:"Hoylake Inn",
+        desc:"Where the Thomas Family will be staying",
+        options:{title:"Hoylake Inn"},
+        show:false,
+        onClick:function(){console.log("Clicked!!!!!!!");this.show=!this.show}
+    },{
+        id:"2",
+        latitude:-33.036487,
+        longitude:  27.824580,
+        title:"East London Airport",
+        desc:"Flying in and out of here",
+        options:{title:"East London Airport"},
+        show:false,
+        onClick:function(){console.log("Clicked!!!!!!!");this.show=!this.show}
+    },{
+        id:"3",
+        latitude:-32.839027,
+        longitude:  28.077665,
+        title:"Inkwenkwezi",
+        desc:"Inkwenkwezi private game reserve is <br>where the reception will take place",
+        options:{title:"Inkwenkwezi Private Game Reserve"},
+        show:false,
+        onClick:function(){console.log("Clicked!!!!!!!");this.show=!this.show}
+    },{
+        id:"4",
+        latitude:-32.972707,
+        longitude:  27.882338,
+        title:"St Marks Anglican Church",
+        desc:"The church where the ceremony will take place",
+        options:{title:"St Marks Anglican Church"},
+        show:false,
+        onClick:function(){console.log("Clicked!!!!!!!");this.show=!this.show}
+    }];
+
+
+    uiGmapGoogleMapApi.then(function(maps) {
+        //get current location
+        //update map
+        console.log("mapLoaded in controler");
+    });
+
+
 
 }]);
